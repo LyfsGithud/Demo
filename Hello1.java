bee169b70e1718362c0f6a4e2c3cfe38fe1f12be
lyf <921798203@qq.com>
2018-08-02 11:32:21 +0800

    Test dev branch

--- Hello1.java
+++
@@ -1,9 +0,0 @@
-public class Hello1{
-
-	public static void main(String[] args){
-		System.out.println("Hello Git211qqqqq!");	
-		System.out.println("ho Git211qqqqq!");	
-		System.out.println("ho Git211qqqqq!");
-	}
-
-}
