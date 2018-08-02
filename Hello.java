7ac67f47caa8b0961b688ab7e6eb946fe8814e87
lyf <921798203@qq.com>
2018-08-02 20:27:31 +0800

    Test patch

--- Hello.java
+++ Hello.java
@@ -3,17 +3,9 @@ public class Hello{
 
 	public static void main(String[] args){
 		System.out.println("Hello Git!!");	
-		System.out.println("Hello Git!!");	
-		System.out.println("Hello Git!!");	
-		System.out.println("Hello Git!!");	
-		System.out.println("Hello Git!!");	
-		List list1 = new ArrayList();
-		int i = 0;
-		list1.add(i++);
-		list1.add(i++);
-		list1.add(i++);
-		list1.add(i++);
-		list1.ad
+		System.out.println("测试patch!!");	
+		
+		
 	}
 
 }
