687e7849736561f0187788aeb750b2d53b640b95
lyf <921798203@qq.com>
2018-08-02 15:58:04 +0800

    Change Hello.java in dev

--- Hello.java
+++ Hello.java
@@ -1,7 +1,14 @@
+import java.util.*;
 public class Hello{
 
 	public static void main(String[] args){
 		System.out.println("Hello Git!!");	
+		List list1 = new ArrayList();
+		int i = 0;
+		list1.add(i++);
+		list1.add(i++);
+		list1.add(i++);
+		list1.add(i++);
 	}
 
 }
