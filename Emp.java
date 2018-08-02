e440c321ac76e2a50910e89236a2c0dc797be07f
lyf <921798203@qq.com>
2018-08-03 07:31:44 +0800

    Change the Hello.java & Emp.java in dev by gitpro

--- Emp.java
+++ Emp.java
@@ -2,6 +2,11 @@ public class Emp{
 
 	public static void main(String[] args){
 		System.out.println("Hello Git111111111!!");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
 	}
 
 }
--- Hello.java
+++ Hello.java
@@ -4,7 +4,11 @@ public class Hello{
 	public static void main(String[] args){
 		//这个就完全是master的Hello.java
 		System.out.println("测试patch!!");	
-		
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+		System.out.println("由gitpro的dev修改的");	
+
 		
 	}
 
