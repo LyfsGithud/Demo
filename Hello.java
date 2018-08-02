47985a621136cd96c7eca43a86f7b780b77f215f
lyf <921798203@qq.com>
2018-08-02 16:41:38 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -3,6 +3,10 @@ public class Hello{
 
 	public static void main(String[] args){
 		System.out.println("Hello Git!!");	
+		System.out.println("Hello Git!!");	
+		System.out.println("Hello Git!!");	
+		System.out.println("Hello Git!!");	
+		System.out.println("Hello Git!!");	
 		List list1 = new ArrayList();
 		int i = 0;
 		list1.add(i++);
