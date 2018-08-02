a618c6fe5842f8ca7a4a81e05440be9256a968c5
lyf <921798203@qq.com>
2018-08-02 16:04:34 +0800

    Change Hello.java in master

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ public class Hello{
 
 	public static void main(String[] args){
 		System.out.println("Hello Git!!");	
+		double dd = 0.1
+		for(int i = 0; i < 1000; i++) {
+			dd *= dd;
+		}
+		System.out.println(dd);
 	}
 
 }
